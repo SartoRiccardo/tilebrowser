1ac089f5010ab9bc407f468542c58c080738429a
SartoRiccardo <rikki.sarto@gmail.com>
2023-11-19 15:49:11 +0100

    feat: Stylized loading and removed Loading

--- ui/Error.js
+++ ui/Error.js
@@ -1,6 +1,6 @@
 const Error = () => {
   return (
-    <div className="error">
+    <div className="error text-4xl text-center">
       There is no tile with that code in that CT event!
     </div>
   );
