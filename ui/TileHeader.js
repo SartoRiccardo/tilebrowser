7997e01ee9c5911d50e35a2ca3758d74d079fe4a
SartoRiccardo <rikki.sarto@gmail.com>
2023-11-19 13:27:59 +0100

    feat: Added challenge type overview

--- ui/TileHeader.js
+++ ui/TileHeader.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { IMG_BANNER, IMG_RELICS } from "./utils/images";
+import { IMG_BANNER, IMG_RELICS, IMG_GAMEMODES } from "./utils/images";
 
 const TileHeader = ({
   ctNum,
@@ -12,11 +12,13 @@ const TileHeader = ({
   const BOSSES = ["Bloonarius", "Lych", "Vortex", "Dreadbloon", "Phayze"];
   const CHAL_TYPES = { 2: "Time Attack", 8: "Least Cash", 9: "Least Tiers" };
 
-  let image = null;
+  let tileImage = null;
   if (tileType == "Banner") {
-    image = <img src={IMG_BANNER} className="tile-type h-[4rem] sm:h-[6rem]" />;
+    tileImage = (
+      <img src={IMG_BANNER} className="tile-type h-[4rem] sm:h-[6rem]" />
+    );
   } else if (tileType != "Regular" || tileType != "TeamFirstCapture") {
-    image = (
+    tileImage = (
       <img
         src={IMG_RELICS[tileType]}
         className="tile-type h-[4rem] sm:h-[6rem]"
@@ -24,19 +26,33 @@ const TileHeader = ({
     );
   }
 
+  let challengeImage = (
+    <img
+      src={IMG_GAMEMODES[chalType]}
+      className="challenge-type pr-4 h-[4rem] sm:h-[6rem] self-center"
+    />
+  );
+
   return (
     <div>
       <div className="flex justify-center">
-        {image}
+        {tileImage}
         <div className="flex">
-          <span className="text-4xl self-center text-center px-4 py-0 my-0">
+          <p className="text-4xl self-center text-center px-4 py-0 my-0">
             CT {ctNum} - Tile {code}
-          </span>
+          </p>
         </div>
       </div>
-      {difficulty} - {gamemode} // {tileType}
-      <br />
-      {chalType > 1000 ? BOSSES[chalType - 1000] : CHAL_TYPES[chalType]}
+      <div className="challenge-type-container w-[80%] mx-auto py-4 my-8 test flex justify-center">
+        {challengeImage}
+        <div className="flex">
+          <p className="self-center text-center text-2xl my-0">
+            {difficulty} - {gamemode}
+            <br />
+            {chalType > 1000 ? BOSSES[chalType - 1000] : CHAL_TYPES[chalType]}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
