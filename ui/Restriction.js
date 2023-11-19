bdaae0faf59344e05d3cbc51524ddf18b546a4d6
SartoRiccardo <rikki.sarto@gmail.com>
2023-11-19 17:32:09 +0100

    fix: Fix Knowledge Disabled appearing where it shouldn't & vice versa

--- ui/Restriction.js
+++ ui/Restriction.js
@@ -1,6 +1,6 @@
 export default ({ name, value }) => {
   if (value === 1) return null;
-  if (name === "Monkey Knowledge Disabled" && value) return null;
+  if (name === "Monkey Knowledge Disabled" && !value) return null;
   if (name === "Selling Disabled" && !value) return null;
 
   return (
