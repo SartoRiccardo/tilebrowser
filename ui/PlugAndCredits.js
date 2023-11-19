4bb2d6a97e4f50b55961cd2f2c5e581531439c79
SartoRiccardo <rikki.sarto@gmail.com>
2023-11-19 16:30:39 +0100

    feat: Added target blank to all links in the footer

--- ui/PlugAndCredits.js
+++ ui/PlugAndCredits.js
@@ -4,23 +4,40 @@ const PlugAndCredits = () => {
       <h2 className="text-3xl text-center my-0">Credits</h2>
       <p className="text-xl my-0">
         App built by{" "}
-        <a href="https://github.com/SartoRiccardo/tilebrowser">Chime</a> | Data
-        reverse engineered by some anonymous guy
+        <a target="_blank" href="https://github.com/SartoRiccardo/tilebrowser">
+          Chime
+        </a>{" "}
+        | Data reverse engineered by some anonymous guy
       </p>
       <br />
       <br />
       <h2 className="text-3xl text-center my-0">Other Resources</h2>
       <p className="text-xl my-0">
-        <a href="https://discord.com/api/oauth2/authorize?client_id=1088892665422151710&permissions=8&scope=bot">
+        <a
+          target="_blank"
+          href="https://discord.com/api/oauth2/authorize?client_id=1088892665422151710&permissions=8&scope=bot"
+        >
           CT Discord Bot
         </a>{" "}
-        | <a href="https://btd6leaderboards.netlify.app/">BTD6 Leaderboards</a>{" "}
-        | <a href="https://sciplypandora.github.io/map/">CT Map Visualizer</a>{" "}
-        <br /> <a href="https://data.ninjakiwi.com">NinjaKiwi Open Data API</a>{" "}
+        |{" "}
+        <a target="_blank" href="https://btd6leaderboards.netlify.app/">
+          BTD6 Leaderboards
+        </a>{" "}
+        |{" "}
+        <a target="_blank" href="https://sciplypandora.github.io/map/">
+          CT Map Visualizer
+        </a>{" "}
+        <br />{" "}
+        <a target="_blank" href="https://data.ninjakiwi.com">
+          NinjaKiwi Open Data API
+        </a>{" "}
         (which this app does NOT use but it would be nice if it could make the
         jump)
         <br />
-        <a href="https://docs.google.com/spreadsheets/d/1C9PHZ1QjiDCbEB-NrR4F7LJgowkm71-492yJOOQvqj8/edit?ouid=105866382831110567560&usp=sheets_home&ths=true">
+        <a
+          target="_blank"
+          href="https://docs.google.com/spreadsheets/d/1C9PHZ1QjiDCbEB-NrR4F7LJgowkm71-492yJOOQvqj8/edit?ouid=105866382831110567560&usp=sheets_home&ths=true"
+        >
           Spreadsheet version
         </a>{" "}
         with an explanation as to how tile generation works
